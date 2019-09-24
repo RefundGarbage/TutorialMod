4ef95bb35064ab094c9a7e78347c2a33706ea438
refundgarbage <stevenxu1202@yahoo.com>
2019-09-23 23:05:29 -0700

    initial setting up

---
+++ src/main/java/org/refundgarbage/TutorialMod/TutorialMod.java
@@ -0,0 +1,7 @@
+package org.refundgarbage.TutorialMod;
+import org.refundgarbage.TutorialMod.util.References;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(References.MOD_ID)
+public class TutorialMod {
+}
---
+++ src/main/java/org/refundgarbage/TutorialMod/util/References.java
@@ -0,0 +1,5 @@
+package org.refundgarbage.TutorialMod.util;
+
+public class References {
+    public static final String MOD_ID = "tutorialmod";
+}
